e1c1086a3e2f5740feb366b2365bbe7740f284f6
kavoronkov <kavoronkov@gmail.com>
2015-06-19 20:57:49 +0300

    add DB_class

---
+++ DB.php
@@ -0,0 +1,20 @@
+<?php
+
+class DB
+{
+    protected static $_instance;
+
+    private function __construct(){}
+
+    private function __clone(){}
+
+    public static function getInstance() {
+        // проверяем актуальность экземпляра
+        if (null === self::$_instance) {
+            // создаем новый экземпляр
+            self::$_instance = new self();
+        }
+        // возвращаем созданный или существующий экземпляр
+        return self::$_instance;
+    }
+}
