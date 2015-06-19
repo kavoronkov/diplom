dc97f4189fd84c469e57c14d6d0178be85e19910
kavoronkov <kavoronkov@gmail.com>
2015-06-19 21:50:26 +0300

    add DB_class

--- DB.php
+++ DB.php
@@ -3,10 +3,13 @@
 class DB
 {
     protected static $_instance;
+    private $_connection;
 
     private function __construct(){}
 
     private function __clone(){}
+
+    //private function __wakeup() {}
 
     public static function getInstance() {
         // проверяем актуальность экземпляра
@@ -16,5 +19,16 @@ class DB
         }
         // возвращаем созданный или существующий экземпляр
         return self::$_instance;
+    }
+
+    public function connect() {
+        try {
+            $_connection_settings = parse_ini_file('config.ini');
+            $this->_connection = new PDO($_connection_settings['db.conn'],
+                                         $_connection_settings['db.user'],
+                                         $_connection_settings['db.pass']);
+        } catch (PDOException $pdo_ex) {
+            throw new PDOException ('Ошибка соединения с базой данных '.$pdo_ex->getMessage());
+        }
     }
 }
