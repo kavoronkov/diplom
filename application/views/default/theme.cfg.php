c684703397892d567de95f0a7b8192d4b6e419af
G-Grand <ggrand.bs@gmail.com>
2015-06-27 11:08:53 +0300

    theme cfg

---
+++ application/views/default/theme.cfg.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    "templates_blocks" => array(
+        "header" => array(
+            "pos" => "1"
+        ),
+        "footer" => array(
+            "pos" => "100"
+        ),
+        "sidebar",
+        "content"
+    ),
+    "routes" => array(
+        "/category/index" => array(
+            "templates_blocks" => array("header","footer","content")
+        )
+    )
+
+);
