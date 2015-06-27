041e7d52edbec0e38fdfd9a7a96531711646a0f8
G-Grand <ggrand.bs@gmail.com>
2015-06-27 10:27:15 +0300

    fileExist fun added

--- core/AutoLoader.php
+++ core/AutoLoader.php
@@ -3,8 +3,20 @@
 class AutoLoader {
     static public function classLoader($className) {
         $classFile = str_ireplace("\\",DIRECTORY_SEPARATOR,$className);
-        if(file_exists($classFile.".php")) {
+        if(self::fileExists($classFile,"php")) {
             spl_autoload($className);
+        }else {
+            echo "NO file!!!";
         }
+    }
+
+    private function fileExists($file,$extension) {
+        $paths = explode(PATH_SEPARATOR,get_include_path());
+        foreach($paths as $path) {
+            if(file_exists($path.$file.".".$extension)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
