2ff74966a22ba8071176528bb94970a7ac72113b
kavoronkov <kavoronkov@gmail.com>
2015-07-08 17:06:31 +0300

    change

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,7 +3,6 @@
 class IndexController implements IController {
 
     public function indexAction() {
-        echo " = = = indexaction";
         $objItemController = new ItemController();
         $objItemController->parseInsertLiga(new SourceModel());
     }
