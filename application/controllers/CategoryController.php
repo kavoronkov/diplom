18e8e50340e496234188a1a26e1163fa9c93958d
kavoronkov <kavoronkov@gmail.com>
2015-07-10 15:37:13 +0300

    change CategoryController

--- application/controllers/CategoryController.php
+++ application/controllers/CategoryController.php
@@ -1,8 +1,24 @@
 <?php
 
-class CategoryController implements IController {
-    public function indexAction() {
-        // TODO: Implement indexAction() method.
+class CategoryController {
+
+    public function createCategoryModel(ModuleModel $objModuleModel, $name) {
+
+        $id = $objModuleModel->getId() . ucfirst($name);
+        $objCategoryModel = new CategoryModel($id, strtoupper($name), $objModuleModel->getId());
+        return $objCategoryModel;
+
     }
 
+    public function insertCategoryModel(CategoryModel $objCategoryModel) {
+
+        $db = DBConnection::getInstance()->_connection;
+
+        $stmt = $db->prepare("INSERT INTO CATEGORY (id, name, idForeign) VALUES (:id, :name, :idForeign)");
+
+        $stmt->execute(array(":id" => $objCategoryModel->getId(),
+                             ":name" => $objCategoryModel->getName(),
+                             ":idForeign" => $objCategoryModel->getIdForeign()));
+
+    }
 }
